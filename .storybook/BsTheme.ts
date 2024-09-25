195bf7df46bdc8c2821f86439fb4c2f3c6bfda21
Pascal Thormeier <pascal.thormeier@liip.ch>
2024-09-25 18:19:19 +0200

    Fix header logo link in theme.

--- .storybook/BsTheme.ts
+++ .storybook/BsTheme.ts
@@ -4,7 +4,7 @@ import storybookCss from '../static/css/storybook-built.css?inline'
 export default create({
   base: 'light',
   brandTitle: 'bs.ch Design system',
-  brandUrl: 'https://kanton-basel-stadt.github.io/designsystem',
+  brandUrl: 'https://kanton-basel-stadt.github.io/storybook',
   brandImage: './logo.png',
   brandTarget: '_self',
   fontBase: "'Inter', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'",

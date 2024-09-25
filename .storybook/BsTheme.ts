a4cbe05ea3246bba8c39721af82096bdafff59d3
Pascal Thormeier <pascal.thormeier@liip.ch>
2024-09-25 18:16:46 +0200

    Fix logo path in theme.

--- .storybook/BsTheme.ts
+++ .storybook/BsTheme.ts
@@ -5,7 +5,7 @@ export default create({
   base: 'light',
   brandTitle: 'bs.ch Design system',
   brandUrl: 'https://kanton-basel-stadt.github.io/designsystem',
-  brandImage: '/logo.png',
+  brandImage: './logo.png',
   brandTarget: '_self',
   fontBase: "'Inter', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'",
   fontCode: 'monospace',

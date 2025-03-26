d663b8e27c63460979965a94fdfd631bbc48529d
Pascal Thormeier <pascal.thormeier@liip.ch>
2025-03-26 17:48:09 +0100

    Refactor icon-getting to look for them recursively.

--- .storybook/src/iconSymbolAddon/withIconsSymbols.ts
+++ .storybook/src/iconSymbolAddon/withIconsSymbols.ts
@@ -2,27 +2,46 @@ import { useChannel } from '@storybook/preview-api'
 import type { DecoratorFunction } from '@storybook/types'
 import { WebComponentsRenderer } from '@storybook/web-components'
 
+type NestedValues = {
+  values?: NestedValues[] | string[] | undefined
+}
+
 export const withIconsSymbols: DecoratorFunction<WebComponentsRenderer> = (
   storyFn
 ) => {
   const emit = useChannel({})
 
-  const storyFnOutput = storyFn()
+  const storyFnOutput: ReturnType<DecoratorFunction<WebComponentsRenderer>> &
+    NestedValues = storyFn()
 
   function onlyUnique<T>(value: T, index: number, array: T[]) {
     return array.indexOf(value) === index
   }
 
-  // @ts-expect-error We know it's there because we checked.
-  const icons: string[] = storyFnOutput.values
-    .map((v: { values: string[] }) => v.values)
-    .filter((v: string[]) => v && v.length > 0)
-    .map((v: { values?: string[] }[]) => (v[0].values ? v[0].values[0] : ''))
-    .filter((e: string) => e && e.startsWith('<icon-symbol-'))
-    .map((i: string) => i.replace(/<icon-symbol-([a-z0-9\-_]+)\s?.*\/>/g, '$1'))
-    .filter(onlyUnique<string>)
+  function getIcons(o: NestedValues) {
+    if (!o.values) {
+      return []
+    }
 
-  emit('icons-symbols/update', { icons })
+    if (o.values.length === 0) {
+      return []
+    }
+
+    if (typeof o.values[0] === 'string') {
+      return o.values
+        .filter((e) => typeof e === 'string' && e.startsWith('<icon-symbol-'))
+        .map((i: string) =>
+          i.replace(/<icon-symbol-([a-z0-9\-_]+)\s?.*\/>/g, '$1')
+        )
+    }
+
+    return o.values
+      .map((e) => getIcons(e))
+      .flat()
+      .filter(onlyUnique<string>)
+  }
+
+  emit('icons-symbols/update', { icons: getIcons(storyFnOutput).flat() })
 
   return storyFnOutput
 }

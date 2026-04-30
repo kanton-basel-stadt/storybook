5631b13d427805e2d920b0154a39de564def992b
Pascal <pascal.steiner@bs.ch>
2026-04-30 14:26:44 +0200

    CCUEW-1885: Refactor Icon and Button stories to enhance customization options and exclude irrelevant icons

--- stories/04_ComponentsForWeb/Buttons/Button.ts
+++ stories/04_ComponentsForWeb/Buttons/Button.ts
@@ -1,5 +1,5 @@
 import { html } from 'lit'
-import { Icon } from './../Icons/Icon'
+import { Icon } from '../Icons/Icon'
 import { classMap } from 'lit-html/directives/class-map.js'
 
 export const Button = (
@@ -7,41 +7,45 @@ export const Button = (
   iconName: string,
   iconPlacement: 'No icon' | 'Icon left' | 'Icon right' | 'Icon only',
   size: 'Standard' | 'Small',
-  types: (
+  decorator: (
     | 'Link'
-    | 'Strong'
-    | 'Limited'
-    | 'Super'
-    | 'Action'
-    | 'Feedback'
     | 'Prev'
     | 'Next'
     | 'Close'
     | 'Add'
     | 'Reload'
     | 'Check'
+  )[],
+  flavour: (
+    | 'Strong'
+    | 'Feedback'
     | 'Success'
     | 'Failure'
+    )[],
+  styles: (
+    | 'Limited'
+    | 'Super'
+    | 'Action'
   )[]
 ) => {
   const classes = {
     button: true,
     'is-sm': size === 'Small',
     'is-icon-only': iconPlacement === 'Icon only',
-    'is-strong': types.includes('Strong'),
-    'is-super': types.includes('Super'),
-    'is-action': types.includes('Action'),
-    'is-feedback': types.includes('Feedback'),
-    'is-prev': types.includes('Prev'),
-    'is-next': types.includes('Next'),
-    'is-close': types.includes('Close'),
-    'is-add': types.includes('Add'),
-    'is-reload': types.includes('Reload'),
-    'is-check': types.includes('Check'),
-    'is-link': types.includes('Link'),
-    'is-limited': types.includes('Limited'),
-    'is-success': types.includes('Success'),
-    'is-failure': types.includes('Failure'),
+    'is-strong': flavour.includes('Strong'),
+    'is-super': styles.includes('Super'),
+    'is-action': styles.includes('Action'),
+    'is-feedback': flavour.includes('Feedback'),
+    'is-prev': decorator.includes('Prev'),
+    'is-next': decorator.includes('Next'),
+    'is-close': decorator.includes('Close'),
+    'is-add': decorator.includes('Add'),
+    'is-reload': decorator.includes('Reload'),
+    'is-check': decorator.includes('Check'),
+    'is-link': decorator.includes('Link'),
+    'is-limited': styles.includes('Limited'),
+    'is-success': flavour.includes('Success'),
+    'is-failure': flavour.includes('Failure'),
   }
 
   return html`
--- stories/04_ComponentsForWeb/Icons/Icon.stories.ts
+++ stories/04_ComponentsForWeb/Icons/Icon.stories.ts
@@ -2,13 +2,15 @@ import { Icon } from './Icon'
 import IconsIndex from '@kanton-basel-stadt/designsystem/dist/configs/icons-index'
 import defineStory from '../../../helpers/defineStory'
 
+const excludedIconNames = ['arrow-east-thin', 'card', 'hinweise_no_rounded', 'map-pin-coloured', 'pin']
+const icons = IconsIndex.iconNames.filter(iconName => !excludedIconNames.includes(iconName))
 const { StoryDefinition, DefaultStory } = defineStory(
   Icon,
   {
     iconName: {
       name: 'Icon name',
       control: 'select',
-      options: IconsIndex.iconNames,
+      options: icons,
     },
   },
   {

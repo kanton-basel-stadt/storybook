8fe76aa0bf1d6eac64c11db4efadc6fe14d23cfe
Pascal <pascal.steiner@bs.ch>
2026-05-04 10:05:27 +0200

    CCUEW-1888: Remove LayoutSection stories and component, update Figma link in Icon stories

--- stories/04_ComponentsForWeb/Icons/Icon.stories.ts
+++ stories/04_ComponentsForWeb/Icons/Icon.stories.ts
@@ -24,7 +24,7 @@ const { StoryDefinition, DefaultStory } = defineStory(
   {
     iconName: 'baselstab',
   },
-  'https://www.figma.com/design/qtexxHxrzUzRg6olooUEmM/BS-Design-System-(DDS)?node-id=24163-12935&t=FznYmIANb8uB5p92-1'
+  'https://www.figma.com/design/qtexxHxrzUzRg6olooUEmM/BS-Design-System--DDS-?node-id=35230-9084&t=LiBXZz2U0289DuOu-0'
 )
 
 export default {

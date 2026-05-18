8b512c9bf7b5e2071fc8090e5d5231f3eb4fb056
Pascal <pascal.steiner@bs.ch>
2026-05-18 19:20:12 +0200

    Fix imports in `HowToIdentifyIcons.mdx` and move `HorizontalLine` to updated folder structure

--- stories/09_Emails/BuildingBlocks/HorizontalLine.ts
+++ stories/09_Emails/BuildingBlocks/HorizontalLine.ts
@@ -2,7 +2,7 @@ import { html } from 'lit'
 
 // prettier-ignore
 export const HorizontalLine = () => html`
-  <p style="border-top:solid 1px #A5A5A5;font-size:1px;margin:0px auto;width:100%;" data-mjml-tag="mj-divider"></p>
+  <p style="border-top:solid 1px #A5A5A5;font-size:1px;margin:0 auto;width:100%;" data-mjml-tag="mj-divider"></p>
   <!--[if mso | IE]>
         <table
            align="center" border="0" cellpadding="0" cellspacing="0" style="border-top:solid 1px #A5A5A5;font-size:1px;margin:0px auto;width:550px;" role="presentation" width="550px"

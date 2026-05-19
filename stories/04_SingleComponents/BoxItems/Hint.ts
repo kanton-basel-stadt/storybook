b0b2a0e65155d3f6dceb99e2c231b93f4ee008be
Pascal <pascal.steiner@bs.ch>
2026-05-19 12:26:01 +0200

    CCUEW-1889: remove empty line before multi hints

--- stories/04_SingleComponents/BoxItems/Hint.ts
+++ stories/04_SingleComponents/BoxItems/Hint.ts
@@ -3,11 +3,14 @@ import { html } from 'lit'
 import { unsafeHTML } from 'lit/directives/unsafe-html.js'
 
 export const Hint = (hints: string[]) => {
+  const hintClasses = 'hyphens-auto whitespace-pre-line break-words'
   return html`
     <div
       class="mb-30 rounded-large border border-gray-300 bg-green-50 p-20 shadow"
     >
-      <div class="grid grid-cols-[auto_1fr] items-center gap-10 text-green-700">
+      <div
+        class="mb-10 grid grid-cols-[auto_1fr] items-center gap-10 text-green-700"
+      >
         <div>
           ${Icon('hinweise_rounded', {
             'size-[20px]': true,
@@ -18,19 +21,13 @@ export const Hint = (hints: string[]) => {
         </div>
       </div>
       ${hints?.length === 1
-        ? html`
-            <p class="hyphens-auto whitespace-pre-line break-words">
-              ${unsafeHTML(hints[0])}
-            </p>
-          `
+        ? html` <p class="${hintClasses}">${unsafeHTML(hints[0])}</p> `
         : hints?.length > 1
           ? html`
               <ul class="ml-10 list-disc pl-6">
                 ${hints.map(
                   (hint) => html`
-                    <li class="hyphens-auto whitespace-pre-line break-words">
-                      ${unsafeHTML(hint)}
-                    </li>
+                    <li class="${hintClasses}">${unsafeHTML(hint)}</li>
                   `
                 )}
               </ul>
